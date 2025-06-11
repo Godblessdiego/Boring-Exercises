4734838280f70cc5782aa18c21ba6ee20b6a5bee
Diego Figueroa <diegofigueroanl@gmail.com>
2025-06-11 15:35:01 -0400

    Solving factorial exercises

---
+++ factorial_number.py
@@ -0,0 +1,10 @@
+def factorial(num):
+    if num == 0:
+        total_multiplication = 1
+        return total_multiplication
+
+    total_product = 1
+    for n in range(num, 0, -1):
+        total_product *= n
+
+    return total_product
