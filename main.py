add1044d3cd34b39ed3c5ad087a348a04972b9fb
Godblessdiego <diegofigueroanl@gmail.com>
2025-07-10 02:26:34 -0400

    agregue la funcion para sacar de binario a decimal

--- main.py
+++ main.py
@@ -18,3 +18,30 @@ class Numero:
 numero1 = Numero(10)
 
 print(numero1.calcular_decimal_binario())
+
+class Binario(Numero):
+    def __init__(self, valor):
+        super().__init__(valor)
+
+    def __str__(self):
+        return self.valor
+
+    def numero_es_binario(self):
+        cadena = str(self.valor)
+        if all(digito in '01' for digito in cadena):
+            return  True
+        else:
+            raise ValueError("El valor no es un número binario válido")
+
+    def calcular_binario_decimal(self):
+        self.numero_es_binario()
+        cadena = str(self.valor)
+        resultado = 0
+        lista = [int(digito) for digito in cadena]
+        for i, digito in enumerate(reversed(lista)):
+            resultado += int(digito) * (2 ** i)
+        return resultado
+
+numero2 = Binario(10)
+
+print(numero2.calcular_binario_decimal())
